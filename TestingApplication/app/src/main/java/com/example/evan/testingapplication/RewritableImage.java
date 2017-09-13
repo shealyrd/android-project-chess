d9439ebc3cbbc2860d7ad3666bded0cf485cf2ae
shealyrd <shealyrd@email.uscupstate.edu>
2017-09-12 22:23:43 -0500

    initial rewritable image

---
+++ TestingApplication/app/src/main/java/com/example/evan/testingapplication/RewritableImage.java
@@ -0,0 +1,14 @@
+package com.example.evan.testingapplication;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Evan on 9/12/2017.
+ */
+
+public class RewritableImage {
+
+    public void createPicture(){
+        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+    }
+}
